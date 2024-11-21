15109da6e41257848e0089ead00a3a00a726dcb3
Pro100Dima1 <dima.sinkin@yandex.ru>
2024-11-21 14:52:01 +0300

    Add test on Selenium "bing"

--- src/test/java/org/example/selenium/MainPageTest.java
+++ src/test/java/org/example/selenium/MainPageTest.java
@@ -1,9 +1,9 @@
 package org.example.selenium;
 
-import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,9 +12,10 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainPageTest {
     private WebDriver driver;
-    private MainPage mainPage;
 
     @BeforeEach
     public void setUp() {
@@ -24,9 +25,8 @@ public class MainPageTest {
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.jetbrains.com/");
+        driver.get("https://www.bing.com/");
 
-        mainPage = new MainPage(driver);
     }
 
     @AfterEach
@@ -34,35 +34,16 @@ public class MainPageTest {
         driver.quit();
     }
 
+    @DisplayName("Проверка поисковой строки в поисковике bing")
     @Test
     public void search() {
-        mainPage.searchButton.click();
-
-        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
+        String input = "Selenium";
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
         searchField.sendKeys("Selenium");
+        searchField.submit();
 
-        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
-        submitButton.click();
 
-        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
+        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
         assertEquals("Selenium", searchPageField.getAttribute("value"));
-    }
-
-    @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.click();
-
-        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
-        assertTrue(menuPopup.isDisplayed());
-    }
-
-    @Test
-    public void navigationToAllTools() {
-        mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click();
-
-        WebElement productsList = driver.findElement(By.id("products-page"));
-        assertTrue(productsList.isDisplayed());
-        assertEquals("All Developer Tools and Products by JetBrains", driver.getTitle());
     }
 }
