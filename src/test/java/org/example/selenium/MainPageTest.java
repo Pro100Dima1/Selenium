c7b02f6529e1ab2c3511d19544e71907aa901788
Pro100Dima1 <dima.sinkin@yandex.ru>
2024-11-27 15:48:03 +0300

    add some code

--- src/test/java/org/example/selenium/MainPageTest.java
+++ src/test/java/org/example/selenium/MainPageTest.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.AbstractDriverOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,6 +26,7 @@ public class MainPageTest {
     @BeforeEach
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
@@ -42,14 +48,21 @@ public class MainPageTest {
         WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
         searchField.sendKeys(input);
         searchField.submit();
+        WebElement searchButton = driver.findElement(By.cssSelector("h2 > a[h='ID=SERP,5165.2']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.visibilityOf(searchButton));
 
+        List<WebElement> href = driver.findElements(By.cssSelector("h2 > a[href] "));
+        searchPage(href,0);
 
-        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
-        assertEquals(input, searchPageField.getAttribute("value"), "Не нашлося");
+        //WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
+       // assertEquals(input, searchPageField.getAttribute("value"), "Не нашлося");
+        assertEquals("https://www.selenium.dev", driver.getCurrentUrl(), "Переход не по первой ссылке списка!!!");
     }
-    @DisplayName("Проверка что первая ссылка в выдаче ведет на https://www.selenium.dev/ ")
-    @Test
-    public void searchPage(){
-        List<WebElement> href = driver.findElements(By.cssSelector("a[h='ID=SERP,5160.2']"));
+    public void searchPage(List<WebElement> href, int num){
+        href.get(num).click();
+        //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        //wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a.selenium-button selenium-webdriver text-uppercase fw-bold")));
+        System.out.println("Нажата первая ссылка из списка");
     }
 }
